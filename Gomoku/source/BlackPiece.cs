071c7d85f0b588702dd1c004fcc4e313e0bbf42d
shiina <daniel910217@gmail.com>
2023-06-20 23:50:42 +0800

    Version 1.0

---
+++ Gomoku/source/BlackPiece.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gomoku
+{
+    class BlackPiece : Piece
+    {
+        public BlackPiece (int x,int y,int z) : base(x, y ,z)
+        {
+            this.Image = Properties.Resources.black;
+        }
+
+        public override PieceType GetPieceType()
+        {
+            return PieceType.BLACK;
+        }
+
+    }
+}
